d64204d7baa98f850198984f1bcd39c82e903a64
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 12:14:59 -0500

    Developer mode

--- packages/backhewksadmin/index.js
+++ packages/backhewksadmin/index.js
@@ -2,28 +2,14 @@
 
 mp.events.add({
   "hewks:add": (player, amount) => {
-    console.log("Amount:" + amount);
-    let currentMoney = player.getVariable("cash");
-    if (currentMoney && currentMoney >= 0 && amount > 0) {
-      currentMoney += amount;
-      player.setVariable("cash", currentMoney);
-    }
-    console.log("New: " + player.getVariable("cash"));
+    console.log("Amount:" + player.__hewksCash);
   },
   "hewks:remove": (player, amount) => {
-    console.log("Amount: " + amount);
-    let currentMoney = player.getVariable("cash");
-    if (currentMoney && currentMoney >= 0 && currentMoney >= amount) {
-      currentMoney -= amount;
-      player.setVariable("cash", currentMoney);
-    }
-    console.log("New: " + player.getVariable("cash"));
+    console.log("Amount:" + player.__hewksCash);
   },
 });
 
 mp.events.add("playerJoin", (player) => {
-  if (player) {
-    player.setVariable("cash", 0);
-  }
-  console.log("Set initial cash");
+  player.__hewksCash = 0;
+  console.log(player.__hewksCash);
 });
