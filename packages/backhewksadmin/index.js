cf5d0269dd2c172a36df821c8af874dce1d76159
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 11:45:44 -0500

    Update admin

--- packages/backhewksadmin/index.js
+++ packages/backhewksadmin/index.js
@@ -1,5 +1,6 @@
 let addMoney = (player, total) => {
   console.log("Perr: Add " + total + " to: " + player.name);
+  mp.game.stats.statSetInt(mp.game.joaat("SP0_TOTAL_CASH"), total, false);
 };
 
 mp.events.add("hewks_addMoney", addMoney);
