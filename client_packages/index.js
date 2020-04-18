64a473c49efe3d58e7dacab990d596726702e05d
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 00:37:45 -0500

    Init the server

---
+++ client_packages/index.js
@@ -0,0 +1,11 @@
+require("login.js");
+require("vspawner/index.js");
+require("weaponcomponents/index.js");
+require("weapontints/index.js");
+require("weaponspawner/index.js");
+require("weaponspawner/weaponData.js");
+require("vspawner/vehicleHashes.js");
+require("speedometer/index.js");
+
+mp.gui.chat.show(true);
+mp.gui.chat.activate(false);
