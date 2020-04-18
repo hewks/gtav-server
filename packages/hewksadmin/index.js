427461ee54484062807b61bec36be0a7b4de564c
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 10:57:37 -0500

    Test server side

--- packages/hewksadmin/index.js
+++ packages/hewksadmin/index.js
@@ -4,4 +4,10 @@ let addMoney = (player, total) => {
   console.log("Add " + total + " to: " + player.name);
 };
 
+function playerJoinHandler(player) {
+  console.log(player.name + " join. Hewks");
+}
+
+mp.events.add("playerJoin", playerJoinHandler);
+
 mp.events.add("hewks_addMoney", addMoney);
