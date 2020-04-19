0e700d442f50e738aa4a234d5bad64b0799d0be5
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 22:21:57 -0500

    Active hospital menu

--- app/client/Factions/cPolice.js
+++ app/client/Factions/cPolice.js
@@ -1,13 +1,12 @@
+const misc = require("../cMisc");
 
-const misc = require('../cMisc');
-
-mp.events.add(
-{
-
-	/* "cHospital-ShowDoctorMenu" : (lang, inject) => {
-		misc.prepareToCef(500);
-		misc.openCef("package://RP/Browsers/Factions/Hospital/interactiveMenu.html", lang);
-		misc.injectCef(inject);
-	}, */
-
+mp.events.add({
+  "cHospital-ShowDoctorMenu": (lang, inject) => {
+    misc.prepareToCef(500);
+    misc.openCef(
+      "package://RP/Browsers/Factions/Hospital/interactiveMenu.html",
+      lang
+    );
+    misc.injectCef(inject);
+  },
 });
