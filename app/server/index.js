534972f25200084d449aab28a17238ff5c1ee8f1
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 19:57:24 -0500

    New structure

---
+++ app/server/index.js
@@ -0,0 +1,16 @@
+require('./Basic/Auth/sLogin');
+require('./Basic/Auth/sRegister');
+require('./Basic/sChat');
+require('./Basic/Money/sATM');
+require('./Business/sBarberShop');
+require('./Business/sClothingShop');
+require('./Business/sCheapCarDealership');
+require('./Business/sCommercialCarDealership');
+require('./Business/sGasStation');
+require('./Jobs/sOrangeCollector');
+require('./Jobs/sMariaCollector');
+require('./Jobs/sCluckinBellCourier');
+require('./Factions/Police/sPolice');
+require('./Basic/sMenu');
+require('./Basic/sGPS');
+require('./3rd/betternotifs.js');
--- client_packages/index.js
+++
@@ -1,5 +0,0 @@
-require("./nativeui/index.js");
-require("./hewksadmin/index.js");
-require("./hewksadmin/print.js");
-require("./speedometer/index.js");
-require("./vspawner/index.js");
