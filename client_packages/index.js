9ea7f7b082247de302b1fe68b452c3bb1d7f2fc1
ctrlprogrammer <sebastianto1999@gmail.com>
2020-04-18 19:45:53 -0500

    Fuck

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,10 +1,5 @@
 require("./nativeui/index.js");
-require("./_rage-console/index.js");
-require("./lsfivem/index.js");
 require("./hewksadmin/index.js");
 require("./hewksadmin/print.js");
 require("./speedometer/index.js");
 require("./vspawner/index.js");
-require("./weaponcomponents/index.js");
-require("./weaponspawner/index.js");
-require("./weapontints/index.js");
